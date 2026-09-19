9f069b5bc9e4eb73171ecdd842457c013f78f1a0
Beta <1213247492@qq.com>
2026-09-19 11:11:21 +0800

    Minor variable renaming

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,10 @@
 //!
 //! # Usage
 //! ```no_run
-//! use elevate_ti::{check_elevation_status, relaunch_as_trusted_installer, ElevationStatus};
+//! use elevate_ti::{check_elevation_status, relaunch_as_trustedinstaller, ElevationStatus};
 //!
 //! if check_elevation_status().unwrap() == ElevationStatus::RequiresElevation {
-//!     relaunch_as_trusted_installer().unwrap();
+//!     relaunch_as_trustedinstaller().unwrap();
 //!     return;
 //! }
 //! ```
